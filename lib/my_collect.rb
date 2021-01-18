9e2811bb58b83b8b325422d694af43d4eb30e216
Daniel Sung <DanielChanSung@gmail.com>
2021-01-18 00:12:29 -0500

    commit tree e29129cdca26770ee7dd67f88da221369e25dc23

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+    i = 0
+    collect = []
+    while i < array.length
+      collect << yield(array[i])
+      i+=1
+    end
+    collect
+  end
